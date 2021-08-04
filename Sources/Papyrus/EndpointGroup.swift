fbc466e2ecf8bad72e5040aa7e176a5f0270933e
Josh Wright <joshuawright11@gmail.com>
2021-08-03 19:27:31 -0700

    Add conversion algorithm

--- Sources/Papyrus/EndpointGroup.swift
+++ Sources/Papyrus/EndpointGroup.swift
@@ -130,7 +130,7 @@ public enum KeyMapping {
     public func mapFrom(input: String) -> String {
         switch self {
         case .snakeCase:
-            return input.camelCaseToSnakeCase()
+            return input.camelCaseFromSnakeCase()
         case .useDefaultKeys:
             return input
         case .custom(_, let fromMapper):
@@ -234,6 +234,52 @@ extension String {
         let result = words.map({ (range) in
             return self[range].lowercased()
         }).joined(separator: "_")
+        return result
+    }
+    
+    fileprivate func camelCaseFromSnakeCase() -> String {
+        let stringKey = self
+        guard !stringKey.isEmpty else { return stringKey }
+
+        // Find the first non-underscore character
+        guard let firstNonUnderscore = stringKey.firstIndex(where: { $0 != "_" }) else {
+            // Reached the end without finding an _
+            return stringKey
+        }
+
+        // Find the last non-underscore character
+        var lastNonUnderscore = stringKey.index(before: stringKey.endIndex)
+        while lastNonUnderscore > firstNonUnderscore && stringKey[lastNonUnderscore] == "_" {
+            stringKey.formIndex(before: &lastNonUnderscore)
+        }
+
+        let keyRange = firstNonUnderscore...lastNonUnderscore
+        let leadingUnderscoreRange = stringKey.startIndex..<firstNonUnderscore
+        let trailingUnderscoreRange = stringKey.index(after: lastNonUnderscore)..<stringKey.endIndex
+
+        let components = stringKey[keyRange].split(separator: "_")
+        let joinedString: String
+        if components.count == 1 {
+            // No underscores in key, leave the word as is - maybe already camel cased
+            joinedString = String(stringKey[keyRange])
+        } else {
+            joinedString = ([components[0].lowercased()] + components[1...].map { $0.capitalized }).joined()
+        }
+
+        // Do a cheap isEmpty check before creating and appending potentially empty strings
+        let result: String
+        if (leadingUnderscoreRange.isEmpty && trailingUnderscoreRange.isEmpty) {
+            result = joinedString
+        } else if (!leadingUnderscoreRange.isEmpty && !trailingUnderscoreRange.isEmpty) {
+            // Both leading and trailing underscores
+            result = String(stringKey[leadingUnderscoreRange]) + joinedString + String(stringKey[trailingUnderscoreRange])
+        } else if (!leadingUnderscoreRange.isEmpty) {
+            // Just leading
+            result = String(stringKey[leadingUnderscoreRange]) + joinedString
+        } else {
+            // Just trailing
+            result = joinedString + String(stringKey[trailingUnderscoreRange])
+        }
         return result
     }
 }
