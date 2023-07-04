ce4020786a82896863918464adb1f63a6f57d552
Jayson Rhynas <jayrhynas@users.noreply.github.com>
2023-07-04 11:21:17 -0400

    Handle path parameter substitution on a per-component basis instead of over the whole string (#10)

---
+++ PapyrusCore/Tests/ParameterTests.swift
@@ -0,0 +1,23 @@
+import XCTest
+@testable import PapyrusCore
+
+final class ParameterTests: XCTestCase {
+    func testPath() {
+        var req = RequestBuilder(baseURL: "foo/", method: "GET", path: "bar/:baz")
+        req.addParameter("baz", value: "value")
+        XCTAssertEqual(try req.fullURL().absoluteString, "foo/bar/value")
+    }
+
+    func testPathPrefix() {
+        var req = RequestBuilder(baseURL: "foo/", method: "GET", path: "bar/:bazzar")
+        req.addParameter("baz", value: "value")
+        XCTAssertThrowsError(try req.fullURL().absoluteString, "foo/bar/value")
+    }
+
+    func testPathCommonPrefix() {
+        var req = RequestBuilder(baseURL: "foo/", method: "GET", path: "bar/:part/:partTwo")
+        req.addParameter("part", value: "valueOne")
+        req.addParameter("partTwo", value: "valueTwo")
+        XCTAssertEqual(try req.fullURL().absoluteString, "foo/bar/valueOne/valueTwo")
+    }
+}
