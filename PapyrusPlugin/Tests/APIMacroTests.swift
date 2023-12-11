8f49dec80dab079cbe90bea28677b2167fdc5e48
Josh Wright <joshuawright11@gmail.com>
2023-12-11 14:44:56 -0800

    Add test

--- PapyrusPlugin/Tests/APIMacroTests.swift
+++ PapyrusPlugin/Tests/APIMacroTests.swift
@@ -283,4 +283,60 @@ final class APIMacroTests: XCTestCase {
             """
         }
     }
+
+    func testJSON() {
+        assertMacro(["API": APIMacro.self]) {
+            """
+            struct User {
+                let id: Int
+                let name: String
+            }
+
+            @API
+            @KeyMapping(.snakeCase)
+            @JSON(encoder: .foo, decoder: .bar)
+            protocol MyService {
+                @POST("users")
+                func getUser(name: String) async throws -> User
+            }
+            """
+        } expansion: {
+            """
+            struct User {
+                let id: Int
+                let name: String
+            }
+            @KeyMapping(.snakeCase)
+            @JSON(encoder: .foo, decoder: .bar)
+            protocol MyService {
+                @POST("users")
+                func getUser(name: String) async throws -> User
+            }
+
+            struct MyServiceAPI: MyService {
+                private let provider: PapyrusCore.Provider
+
+                init(provider: PapyrusCore.Provider) {
+                    self.provider = provider
+                }
+
+                func getUser(name: String) async throws -> User {
+                    var req = builder(method: "POST", path: "users")
+                    req.addField("name", value: name)
+                    let res = try await provider.request(req)
+                    try res.validate()
+                    return try res.decode(User.self, using: req.responseDecoder)
+                }
+
+                private func builder(method: String, path: String) -> RequestBuilder {
+                    var req = provider.newBuilder(method: method, path: path)
+                    req.keyMapping = .snakeCase
+                    req.requestEncoder = .json(.foo)
+                    req.responseDecoder = .json(.bar)
+                    return req
+                }
+            }
+            """
+        }
+    }
 }
