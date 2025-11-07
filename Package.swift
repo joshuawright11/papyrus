63fbe12b859fdc1dd7b66be807f423c3f21d12b3
Josh Wright <joshuawright11@gmail.com>
2025-11-07 15:28:55 -0800

    bump swift-syntax

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         .library(name: "Papyrus", targets: ["Papyrus"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/swiftlang/swift-syntax", from: "600.0.1"),
+        .package(url: "https://github.com/swiftlang/swift-syntax", from: "602.0.0"),
         .package(url: "https://github.com/pointfreeco/swift-macro-testing", from: "0.5.0"),
     ],
     targets: [
